21bb7de9754ca643bdf7c162c23f4b071a956fab
Rega <regha_gaara@yahoo.co.id>
2019-07-29 01:23:21 +0700

    Latihan CRUD

---
+++ app/src/main/java/com/thedemit/mycrud/konfigurasi.java
@@ -0,0 +1,37 @@
+package com.thedemit.mycrud;
+
+public class konfigurasi {
+
+    //Dibawah ini merupakan Pengalamatan dimana Lokasi Skrip CRUD PHP disimpan
+    //Pada tutorial Kali ini, karena kita membuat localhost maka alamatnya tertuju ke IP komputer
+    //dimana File PHP tersebut berada
+    //PENTING! JANGAN LUPA GANTI IP SESUAI DENGAN IP KOMPUTER DIMANA DATA PHP BERADA
+//    public static final String URL_ADD          = "http://192.168.0.13/thedemit/crudAndroid/tambahPgw.php";
+//    public static final String URL_GET_ALL      = "http://192.168.0.13/thedemit/crudAndroid/tampilSemuaPgw.php";
+//    public static final String URL_GET_EMP      = "http://192.168.0.13/thedemit/crudAndroid/tampilPgw.php?id=";
+//    public static final String URL_UPDATE_EMP   = "http://192.168.0.13/thedemit/crudAndroid/updatePgw.php";
+//    public static final String URL_DELETE_EMP   = "http://192.168.0.13/thedemit/crudAndroid/hapusPgw.php?id=";
+
+    public static final String URL_ADD="http://mydaun.co.id/testing/www/APITest/tambahPgw.php";
+    public static final String URL_GET_ALL = "http://mydaun.co.id/testing/www/APITest/tampilSemuaPgw.php";
+    public static final String URL_GET_EMP = "http://mydaun.co.id/testing/www/APITest/tampilPgw.php?id=";
+    public static final String URL_UPDATE_EMP = "http://mydaun.co.id/testing/www/APITest/updatePgw.php";
+    public static final String URL_DELETE_EMP = "http://mydaun.co.id/testing/www/APITest/hapusPgw.php?id=";
+
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Skrip PHP
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_EMP_NAMA = "name";
+    public static final String KEY_EMP_POSISI = "desg"; //desg itu variabel untuk posisi
+    public static final String KEY_EMP_GAJIH = "salary"; //salary itu variabel untuk gajih
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "name";
+    public static final String TAG_POSISI = "desg";
+    public static final String TAG_GAJIH = "salary";
+
+    //ID karyawan
+    //emp itu singkatan dari Employee
+    public static final String EMP_ID = "emp_id";
+}
